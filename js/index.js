cc70ab3f0dde48e19936ced55d5a6f4e93b12206
Andrew Pham <andrewpham92@gmail.com>
2020-09-09 14:01:13 -0400

    first init

--- js/index.js
+++ js/index.js
@@ -1,9 +1,20 @@
 const myButton = document.getElementById('clickMe');
 const noun1 = document.getElementById('noun1');
 const noun1Input = document.getElementById('noun1Input');
+const noun2 = document.getElementById('noun2');
+const noun2Input = document.getElementById('noun2Input');
+const verb1 = document.getElementById('verb1');
+const verb1Input = document.getElementById('verb1Input');
+const verb2 = document.getElementById('verb2');
+const verb2Input = document.getElementById('verb2Input');
 
 myButton.addEventListener('click', function (event) {
   event.preventDefault();
 
   noun1.innerHTML = noun1Input.value;
+  noun2.innerHTML = noun2Input.value;
+  verb1.innerHTML = verb1Input.value;
+  verb2.innerHTML = verb2Input.value;
+
 });
+
